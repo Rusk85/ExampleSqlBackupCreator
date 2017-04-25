4635af0380b6325b5741d977dc3cc33293c66c9c
Rusk85 <troggs@gmx.net>
2017-04-26 00:19:37 +0200

    Some cleaning up

--- SqlBackup.GUI/Form1.cs
+++ SqlBackup.GUI/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SqlBackup.GUI
@@ -22,9 +15,7 @@ namespace SqlBackup.GUI
             var sqlBackupCreator = new SqlBackupCreator();
             var conStr = tbConStr.Text;
             if (conStr == null)
-            {
                 return;
-            }
             sqlBackupCreator.DoBackup(conStr);
         }
     }
